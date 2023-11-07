826881923a34265ed4850a7a3a3bf248ba7b7f0b
satyam4p <satyamkumar343@gmail.com>
2023-11-07 14:57:13 +0530

    feat: added color in input

--- src/Input.js
+++ src/Input.js
@@ -11,19 +11,34 @@ const REGEX = /({{.*?}})/g;
 
 const InputField = ()=>{
   const [value, setValue] = useState('');
-
-  useEffect(()=>{
+  const ref = useRef();
+  useEffect(()=>{    
+    console.log("split:: ",value.split(REGEX));
 
   },[value])
+  const color = "red";
+  const syncScroll=(e)=>{
+    ref.current.scrollTop = e.target.scrollTop;
+    ref.current.scrollLeft = e.target.scrollLeft;
+  }
 
   return(
     <>
       <input
           value={value}
           onChange={(e) => setValue(e.target.value)}
+          onScroll = {syncScroll}
         />
-        <div className="input-renderer">
-          
+        <div ref={ref} className="input-renderer">
+          {value.split(REGEX).map((word, index)=>{
+            if(word.match(REGEX) !== null){
+              return(
+                <span key={index} style={{color:`${color}`}}>{word}</span>
+              )
+            }else{
+              return <span key={index}>{word}</span>
+            }
+          })}
         </div>
     </>
   )
