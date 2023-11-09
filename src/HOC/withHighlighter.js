0c8282afd946afc92e04ce431928e2186fda35f8
satyam4p <satyamkumar343@gmail.com>
2023-11-09 22:43:58 +0530

    feat: artial changes for suggestions

--- src/HOC/withHighlighter.js
+++ src/HOC/withHighlighter.js
@@ -1,18 +1,16 @@
 import React,{ useRef } from "react";
+import './highlighter.css';
 const suggestions = [
   "abcde",
   "test",
   "test2"
 ]
 
-
-
 function withHighlighter(WrappedComponent){
   return (props)=>{
     const {value, color} = props;
     const ref = useRef();
     const REGEX = /({{.*?}})/g;
-    console.log("split val:: ",value.split(REGEX));
     return(
       <>
       <WrappedComponent {...props} highlighterRef = {ref}/>
@@ -20,13 +18,23 @@ function withHighlighter(WrappedComponent){
           {value.split(REGEX).map((word, index)=>{
             if(word.match(REGEX) !== null){
               return(
-                <span key={index} style={{color:`${color}`}}>{word}</span>
+                <span className="highlighter" key={index} style={{color:`${color}`}}>{word}</span>
               )
             }else{
               return <span key={index}>{word}</span>
             }
           })}
       </div>
+      <ul className="suggestions-container">
+        {suggestions.map((item, index)=>{
+          return(
+            <li className="highlighter-suggestion">
+              {item}
+              <hr/>
+            </li>
+          )
+        })}
+      </ul>
       </>
     )
 
