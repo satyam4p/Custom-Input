5780d5c07d6b98e922b7a9754afd30fbf283a0f2
satyam4p <satyamkumar343@gmail.com>
2023-11-14 21:54:59 +0530

    WIP: suggestions changes

--- src/HOC/hasSuggestions.js
+++ src/HOC/hasSuggestions.js
@@ -1,25 +1,66 @@
+  import { useEffect, useState } from 'react';
 import './highlighter.css';
 
 const hasSuggestions = (WrappedComponent)=>{
   return (props)=>{
-    const { hasSuggestion, value, onChange, suggestions, REGEX } = props;
+    const { hasSuggestion, value, onChange, suggestions, REGEX, spanRef, wordRef } = props;
+    const [suggest, setSuggest] = useState(suggestions);
+    const [showSuggest, setShow] = useState(false);
+    useEffect(()=>{
+      console.log("wordRef:: ",wordRef)
+    },[value])
+
     const setSuggestion = (event, item) =>{
+      setShow(false);
       if(value.length > 0){
         let splitVal = value.split(REGEX);
+        console.log(splitVal)
         let replaceVal = splitVal[splitVal.length - 2];
         var regex = /{{(.*?)}}/g;
-        let ans = replaceVal ? replaceVal.replace(regex, `{{${item.name}}}`) : value;
-        const finalVal = splitVal.slice(0,splitVal.length - 2).join('') + ans;
+        let ans = replaceVal ? replaceVal.replace(regex, `{{${item.name}}}`) : "";
+        const finalVal = splitVal.slice(0,splitVal.length - 2).join('') + ans + splitVal.slice(splitVal.length-1, splitVal.length);
         onChange(finalVal);
-      } 
+      }
+
+      
     }
+
+    useEffect(()=>{
+
+      if(value){
+        value.split(REGEX).map((word, index)=>{
+          if(word.match(REGEX) != null){
+            let split1 = word.split("{{")[1]
+            let searchVal = split1.split("}}")[0];
+            let suggest = suggestions.filter((w,index)=>{
+              return w.name.includes(searchVal);
+            });
+            setSuggest(suggest);
+          }
+        })
+      }
+      if(value.match(/({{)/g)){
+        setShow(true);
+      }
+    },[value])
+    useEffect(()=>{
+
+      // if(value.match(/({{.*?)/g)){
+      //   if(value.split(/({{.*?)/g)[value.split(/({{.*?)/g).length-2] == '{{'){
+      //     setShow(true)
+      //   }else{
+      //     setShow(false);
+      //   }
+      // }
+
+    },[value])
 
     if(hasSuggestion){
       return(
         <>
         <WrappedComponent {...props} />
         <ul className="suggestions-container">
-          {suggestions.map((item, index)=>{
+          { showSuggest && suggest.map((item, index)=>{
             return(
               <li onClick={e=>setSuggestion(e, item)} key={index} className="highlighter-suggestion">
                 {item.name}
--- src/HOC/withHighlighter.js
+++ src/HOC/withHighlighter.js
@@ -1,21 +1,33 @@
 import React,{ useRef } from "react";
 
+const SpanElem = (props)=>{
+  console.log(props)
+  return(
+    <span className="highlighter" style={{color:`${props.color}`}}>{props.value}</span>
+  )
+
+}
+
 function withHighlighter(WrappedComponent){
   return (props)=>{
     const {value, color} = props;
     const ref = useRef();
+    const spanref = useRef();
+    const wordRefs = useRef([]);
     const REGEX = /({{.*?}})/g;
     return(
       <>
-      <WrappedComponent {...props} highlighterRef = {ref} REGEX = {REGEX}/>
+      <WrappedComponent {...props} highlighterRef = {ref} REGEX = {REGEX} wordRef = {wordRefs.current}/>
       <div ref={ref} className="input-renderer">
           {value.split(REGEX).map((word, index)=>{
             if(word.match(REGEX) !== null){
               return(
-                <span className="highlighter" key={index} style={{color:`${color}`}}>{word}</span>
+                <div  key={index}>
+                <span className="highlighter" ref={elem =>wordRefs.current[index] = elem} key={index} style={{color:`${color}`}}>{word}</span>
+                </div>
               )
             }else{
-              return <span key={index}>{word}</span>
+              return <span  ref={spanref} key={index}>{word}</span>
             }
           })}
       </div>
